3598440d420d47ed791f4948f0572cf11050989e
SabrinaStoekly <sstoekly@gmail.com>
2023-12-09 22:20:24 +0000

    Added checkout HTML template and completed link setup for checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Your bag is empty at the moment")
+        return redirect(reverse('products:all_products'))
+
+    context = {
+        'order_form': OrderForm(),
+    }
+
+    return render(request, 'checkout/checkout.html', context)
+
+
