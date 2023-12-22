f849d96d13437f3bfadc11dc3273f9c8e9d16ee1
SabrinaStoekly <sstoekly@gmail.com>
2023-12-22 18:09:26 +0000

    creating forms.py

--- products/forms.py
+++ products/forms.py
@@ -1,21 +1,21 @@
 from django import forms
-from .models import Product, Category
 from .widgets import CustomClearableFileInput
+from .models import Product, Category
+
 
 class ProductForm(forms.ModelForm):
-    image = forms.ImageField(
-        label='Image',
-        required=False,
-        widget=CustomClearableFileInput
-    )
-
-    class Meta:
-        model = Product
-        fields = '__all__'
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['category'].queryset = Category.objects.all()
+        self.set_category_choices()
+        self.apply_css_classes()
 
+    def set_category_choices(self):
+        categories = Category.objects.all()
+        friendly_names = [(category.id, category.get_friendly_name()) for category in categories]
+        self.fields['category'].choices = friendly_names
+
+    def apply_css_classes(self):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
