7f1a6f4ba4040aa10855e1d70b237333a4f18ac0
SabrinaStoekly <sstoekly@gmail.com>
2023-12-24 17:23:09 +0000

    addig allow host

--- setup/settings.py
+++ setup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#lwl4y-^#ch93ebo=8r5du@s)s70r(8txhwcle@rh**#2$=b03
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['luminashop.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['luminashop.herokuapp.com', 'luminashop-43e0933e7995.herokuapp.com', 'localhost']
 
 # ALLOWED_HOSTS = ['8000-sabrinastoekly-lumina-ph4e6plt1xp.ws-eu107.gitpod.io']
 
