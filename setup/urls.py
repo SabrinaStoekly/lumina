19622f9e6264c73afe2987dc485c67f8339fe17f
SabrinaStoekly <sstoekly@gmail.com>
2023-12-30 15:39:25 +0000

    finising favicon

--- setup/urls.py
+++ setup/urls.py
@@ -30,8 +30,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
-    path('profile/', include('profiles.urls')),
-    path('favicon.ico', RedirectView.as_view(url=settings.MEDIA_URL + 'favicon_io/favicon.ico')),
+    path('profile/', include('profiles.urls')),    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'home.views.custom_404_page'
